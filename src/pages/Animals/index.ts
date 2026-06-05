b4f6cf592702982aeff611b150578b977fd571f4
bacharbh <bachar777benhassine@gmail.com>
2026-06-05 06:46:06 +0100

    yrd

--- src/pages/Animals/index.ts
+++ src/pages/Animals/index.ts
@@ -1,4 +1,3 @@
 export { default as Animals } from './Animals';
-export { default as AnimalDetail } from './AnimalDetail';
 export { default as AnimalProfile } from './AnimalProfile';
 export * from './components';
